3c1b25938f99cdc0b2fa08d1b18eeda58d004948
erich <erich@mac.com>
2015-03-04 15:33:36 -0500

    Quick example web app.

--- webapp/server.R
+++ webapp/server.R
@@ -3,11 +3,29 @@
 ## REQUIRE
 require(shiny)
 
+figuresList <- figureFiveBCDE()
+
 ## SERVER LOGIC
 shinyServer(function(input, output) {
   output$figureA <- renderPlot({
     # Take a dependency on input$goButton
     input$runCode
     figureFiveA()
+  })
+  
+  output$figureB <- renderPlot({
+    figuresList$gradeBoxPlot
+  })
+  
+  output$figureC <- renderPlot({
+    figuresList$lymphNodeBoxPlot
+  })
+  
+  output$figureD <- renderPlot({
+    figuresList$timeBoxPlot
+  })
+  
+  output$figureE <- renderPlot({
+    figuresList$clincalBoxPlot
   })
 }) 
--- webapp/ui.R
+++ webapp/ui.R
@@ -6,72 +6,57 @@ require(shiny)
 ## CLIENT SIDE UI
 shinyUI(pageWithSidebar(
   # Application Title
-  headerPanel('Demonstration Afor "FRDS-Powered App"'),
+  headerPanel('Demonstration of an "FRDS-Powered App"'),
   
   # Sidebar with controls to select a patient
   sidebarPanel(
     actionButton(inputId = 'runCode', label = 'Run Code'),
-    
     br(),
-    
     h5('Explanation'),
-    
-    helpText('Select a patient from the queue using the drag menu above.',
-             'When you release, the web application will generate',
-             'a range of predictions for the patient based on several',
-             'predictive models.'),
-    
-    helpText('Click on the tabs to see other visualizations or analyses.'),
-    
-    helpText('For the model "neighborhood", 25 random',
-             'test splits of the data were performed. Using the "Random Forest"',
-             'ensemble machine learning method, we generate 25 models that each "see"',
-             'something different about the full cohort and thereby create',
-             'a range of predictions for each patient--a neighborhood.',
-             'No machine learning method is perfect;',
-             'so by training multiple models on different cuts of the patient cohort',
-             'you are inducing real world variability. If the range of',
-             'predictions for a particular patient is tight, we have reasonable',
-             'confidence that the predictions are reliable. If they are widely dispersed,',
-             'this is indicative of uncertainty in the predictions.'),
-    
-    helpText('The features on which the models are trained include demographics, surgeon,',
-             'BMI, pre-op labs, operative variables, laparoscopic vs. open, diagnosis,',
-             'year of procedure, functional status, among others.'),
-    
-    helpText('In this web application, for each patient selected, some',
-             'metadata is displayed in the upper panel while',
-             'their out-of-sample predictions are plotted above a density',
-             'function plot of the cumulative "landscape" of predictions for all',
-             'patients across the entire neighborhood of models'),
-    
+    helpText('Taking Figure 5 from the Science Translational Medicine Paper,',
+             'here we are now showing how one could use a FRDS API to create',
+             'an "App". Using webservices, one is pulling Provenance, Data, and',
+             'Code and creating a user-friendly web application to use the FRDS',
+             'APIs to execute an analysis and generate figures'),
     br(),
+    helpText('So just as Apple making the iOS SDK available to developers spawned',
+             'millions of iPhone and iPad applications, the FRDS APIs will facilitate',
+             'applications for research data that are only limited by investigators',
+             'creativity'),
     
     h5('Web Application by:'),
-    helpText('Erich S. Huang, MD, PhD') 
+    helpText('Erich S. Huang, MD, PhD'),
+    helpText('Division of Translational Bioinformatics'),
+    helpText('Department of Biostatistics & Bioinformatics')
   ),
   
   mainPanel(
     tabsetPanel(
       tabPanel('Figure 5, Panel A',
                h4('ANOVA analysis of clinical variables and model performance'),
-               plotOutput("figureA", width = '85%', height = '700px')),
+               helpText('Percentage of Concordance Index variance explained by each clinical variable.'),
+               plotOutput("figureA", width = '85%', height = '500px')),
+      
       tabPanel('Figure 5, Panel B',
-               h4('Foo'),
-               # plotOutput("figureB", width = '85%', height = '400px'),
-               br(),
-               helpText('Bar.')),
+               h4('Prognostic model performance and histologic grade'),
+               helpText('Concordance Indices were calculated for OsloVal models according to subsets of patients by histological grade.'),
+               plotOutput("figureB", width = '85%', height = '500px')),
+      
       tabPanel('Figure 5, Panel C',
-               h4('Foo'),
-               helpText('Bar.')),
+               h4('Prognostic model performance and lymph node status'),
+               helpText('Concordance Indicies were calculated for OsloVal models according to subsets of patients by LN status.'),
+               plotOutput("figureC", width = '85%', height = '500px')),
+      
       tabPanel('Figure 5, Panel D',
-               h4('Foo'),
-               helpText('Bar')
-      ),
+               h4('Prognostic model performance and follow-up time'),
+               helpText('Concordance Indices were calculated for OsloVal models according to subsets of patients by follow-up time (OS)'),
+               plotOutput("figureD", width = '85%', height = '500px')),
+      
       tabPanel('Figure 5, Panel E',
-               h4('Foo'),
-               helpText('Bar.')
-      )
+               h4('Prognostic model performance and clinical characteristics'),
+               helpText('CIs were calculated for OsloVal models according to subsets of patients by age, ER status, and HER2 status. Patients were divided into subsets according to each of the above clinical characteristics. Individual model predictions were generated for patients belonging to each subset, and the CI was calculated by comparison with the actual survival for each patient.'),
+               plotOutput("figureE", width = '85%', height = '500px'))
     )
   )
-))
+)
+)
